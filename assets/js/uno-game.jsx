0624946b36278b093909f4173ad557424e042b73
l-tn3314 <l.tn3314@gmail.com>
2019-02-17 17:18:30 -0500

    server-side logic, js stubs to test logic

--- assets/js/uno-game.jsx
+++ assets/js/uno-game.jsx
@@ -11,17 +11,41 @@ class UnoGame extends React.Component {
   constructor(props) {
     super(props);
 
-  this.channel = props.channel;
+    this.channel = props.channel;
 
-  this.channel
-      .join()
-      .receive("ok", resp => { console.log("Joined successfully", resp) })
-      .receive("error", resp => { console.log("Unable to join", resp) });
+    this.channel
+        .join()
+        .receive("ok", this.set_view.bind(this))
+        .receive("error", resp => { console.log("Unable to join", resp) });
   }
 
+  set_view(view) {
+    console.log("Joined successfully");
+    console.log(view.game);
+    this.setState(view.game);
+  }
+
+  drawCard() {
+    this.channel.push("draw_card", { playerid: 1 })
+        .receive("ok", (resp) => { this.setState(resp.game); 
+      console.log("draw_card...");
+      console.log(resp.game); });
+  }
+
+  playCard() {
+    let card = this.state.player_hands[1][0];
+    console.log("player hands...");
+    console.log(this.state.player_hands[1]);
+    this.channel.push("play_card", { playerid: 1, card: card})
+        .receive("ok", (resp) => { this.setState(resp.game); console.log(resp.game); });
+  }
 
   render() {
-    return <h1>Hello World</h1>
+    return <div>
+        <h1>Hello World</h1>
+        <button onClick={this.drawCard.bind(this)}>Draw</button>
+        <button onClick={this.playCard.bind(this)}>Play card</button>
+      </div>
   }
 
 }
